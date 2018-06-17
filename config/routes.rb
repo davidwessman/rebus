14108af1179a9be373f0614cfbdc65f99ada944b
David Wessman <david.wesmn@gmail.com>
2018-06-17 12:31:54 +0200

    Put posts on root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources(:posts, only: :show) do
+  resources(:posts, only: :show, path: '/') do
     resources(:questions, only: %i[show update], path: :q)
   end
 
